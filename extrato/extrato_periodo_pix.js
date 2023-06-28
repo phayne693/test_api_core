1e98293e2dc54068115301470480e0780634b020
Jeferson Queiroz <phayne693@gmail.com>
2023-06-28 13:55:06 -0300

    extrato completo

---
+++ extrato/extrato_periodo_pix.js
@@ -0,0 +1,32 @@
+import supertest from "supertest";
+import { tk_project } from "../helpers/token_project.js";
+import { tk_user } from "../helpers/auth_simples.js";
+import { user_id } from "../helpers/auth_simples.js";
+
+
+const request = supertest('https://api.norwaydigital.com.br/prod/v1/');
+
+describe('Extrato Pix Periodo', () => {
+
+    let token_project
+    let token_user
+    let id_user
+
+    before(async() =>{
+        token_project = await tk_project()
+        token_user = await tk_user()
+        id_user = await user_id()
+    })
+
+
+    it('GET extracts/user/userId/dates/?inicialDate=2023-01-01&finalDate=2023-06-28&pix-true', () => {
+
+        request.get(`extracts/user/${id_user}/dates/?inicialDate=2023-01-01&finalDate=2023-06-28&pix-true`).set('tk-project', token_project).set('tk-user', token_user).then((res) => {
+            console.log(res.body)
+        })
+
+    })
+
+
+
+})
