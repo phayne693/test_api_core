d5663e29d255de27c4e3bed42dd6468e9eaaa9d7
Jeferson Queiroz <phayne693@gmail.com>
2023-07-18 17:44:51 -0300

    ajuste

--- pix/qr-copia-cola/consulta_qr.js
+++ pix/qr-copia-cola/consulta_qr.js
@@ -1,5 +1,5 @@
 import { tk_project } from "../../helpers/token_project.js";
-import { tk_user, user_id } from "../../helpers/auth_simples.js";
+import { tk_user, user_id } from "../../helpers/auth_acc_thiago.js";
 import { geracao_qr_dinamico } from "../../helpers/qr_dinamico_code.js";
 import supertest from "supertest";
 
@@ -17,13 +17,13 @@ describe('Consulta QR', ()=>{
         token_project = await tk_project()
         token_user = await tk_user()
         data = {
-            userId: id_user = await user_id(),
-            documento: qr_document = await geracao_qr_dinamico()
-        }
-        // console.log(qr_document)
-        // console.log(id_user)
+            "userId": id_user = await user_id(),
+            "documento": qr_document = await geracao_qr_dinamico()
+          }
+        console.log(qr_document)
+        console.log(id_user)
     })
-
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
     it('POST pix/qraleatorio', ()=>{
         request.post('pix/qraleatorio').set('tk-project', token_project).set('tk-user', token_user).send(data).then((res) =>{
             console.log(res.body)
